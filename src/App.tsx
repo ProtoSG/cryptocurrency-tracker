de5d47ce97d8ef81026c504fb357e6ff3fda3e6b
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-11-03 11:19:10 -0500

    feat: add CryptoProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import ChartCrypto from "./modules/chartCrtypo/ChartCrypto"
-import FavoriteCryptos from "./modules/favoriteCryptos/FavoriteCryptos"
-import ListCryptos from "./modules/listCryptos/ListCryptos"
+import ChartCrypto from "./modules/chartCrtypo/ChartCrypto";
+import { CryptoProvider } from "./modules/core/hooks/useCrypto";
+import FavoriteCryptos from "./modules/favoriteCryptos/FavoriteCryptos";
+import ListCryptos from "./modules/listCryptos/ListCryptos";
 
 function App() {
+
   return (
-    <main className="md:flex h-dvh">
-      <ListCryptos />
-      <div className="w-full">
-        <ChartCrypto />
-        <FavoriteCryptos />
-      </div>
-    </main>
+    <CryptoProvider>
+      <main className="md:flex h-dvh ">
+        <ListCryptos />
+        <div className="flex-1 flex flex-col w-[60%]">
+          <ChartCrypto />
+          <FavoriteCryptos />
+        </div>
+      </main>
+    </CryptoProvider>
   )
 }
 
