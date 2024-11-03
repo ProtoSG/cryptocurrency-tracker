f73a2b7f2491803a2ef89f4b14100f0dfbea7b87
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-11-03 11:18:48 -0500

    feat: update ListCrypto module

--- src/modules/listCryptos/ListCryptos.tsx
+++ src/modules/listCryptos/ListCryptos.tsx
@@ -1,25 +1,31 @@
-import { useCrypto } from "../core/hooks/useCrypto";
+import { useEffect } from "react";
+import { Box } from "../../components";
+import { useCrypto, useCryptoSelected } from "../core/hooks/useCrypto";
 import ItemCrypto from "./components/ItemCrypto";
 import SearchCrypto from "./components/searchCrypto";
 
 export default function ListCryptos() {
 
-  const { cryptos } = useCrypto();
+  const { loading } = useCrypto()
+  const { cryptos } = useCryptoSelected();
+
+
+  if (loading) return <p>Loading...</p>
 
   return (
-    <section className="w-full lg:w-96 md:w-32  lg:min-w-96 py-8 border-r-2 border-gray-400/40">
-      <div className="h-28">
-        <h1 className="md:hidden lg:block text-pretty mb-6 px-8 text-4xl font-semibold">Crypto Tracker</h1>
+    <section className="flex-none min-w-[480px] w-[480px] p-8 flex flex-col gap-8">
+      <Box className="flex flex-col gap-4">
+        <h1 className="md:hidden lg:block text-pretty text-2xl font-semibold">Crypto Tracker</h1>
         <h1 className="hidden md:block lg:hidden text-pretty text-center mb-6 px-8 text-4xl font-semibold">C T</h1>
         <SearchCrypto />
-      </div>
-      <section className="flex flex-col gap-4 py-6 h-96  md:h-[90%]  overflow-scroll">
+      </Box>
+      <Box className="flex w-full flex-col pt-0 pb-0 pr-0 pl-0 h-96 md:h-[90%] overflow-y-scroll">
         {
           cryptos.map((crypto) => (
             <ItemCrypto key={crypto.id} crypto={crypto} />
           ))
         }
-      </section>
+      </Box>
     </section>
   )
 }
