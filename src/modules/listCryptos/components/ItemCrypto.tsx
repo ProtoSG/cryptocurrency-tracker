784df5bc68a1ce348aff6b24c272fcde02f9bf0a
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-11-03 11:18:33 -0500

    feat: add ItemCrypto and SearchCrypto components

--- src/modules/listCryptos/components/ItemCrypto.tsx
+++ src/modules/listCryptos/components/ItemCrypto.tsx
@@ -1,5 +1,8 @@
+import { useCallback, useMemo } from "react";
 import DolarIcon from "../../../assets/icons/DolarIcon";
-import { useCrypto } from "../../core/hooks/useCrypto";
+import { TrendingDownIcon } from "../../../assets/icons/TrendingDownIcon";
+import { TrendingUpIcon } from "../../../assets/icons/TrendingUpIcon";
+import { useCrypto, useCryptoSelected } from "../../core/hooks/useCrypto";
 import { DataItem } from "../../core/model/data.mode";
 
 interface ItemCryptoProps {
@@ -8,32 +11,35 @@ interface ItemCryptoProps {
 
 export default function ItemCrypto({ crypto }: ItemCryptoProps) {
 
-  const { cryptoSelected, setCryptoSelected } = useCrypto();
+  const { cryptoSelected, setCryptoSelected } = useCryptoSelected();
 
   const selectCrypto = () => {
     setCryptoSelected(crypto)
-  }
+  };
+
 
   return (
     <article
       onClick={() => selectCrypto()}
-      className={`flex items-center transition-colors px-8 py-2 hover:cursor-pointer hover:bg-zinc-700 
-    ${cryptoSelected?.id === crypto.id ? 'bg-zinc-700' : ''}`}>
-      <div className="flex justify-between w-full pr-4 md:justify-center lg:justify-between">
+      className={`flex items-center transition-colors gap-4 px-8 py-4 hover:cursor-pointer hover:bg-zinc-700 
+    ${cryptoSelected?.id === crypto.id ? 'bg-zinc-700 sticky top-0' : ''}`}>
+      <div className="flex justify-between w-full  md:justify-center lg:justify-between">
         <div className="flex flex-col md:items-center lg:items-start ">
           <span className="text-xl">{crypto.symbol}</span>
           <small className="text-zinc-400 md:text-center">{crypto.name}</small>
         </div>
-        <span className="flex md:hidden lg:flex text-2xl items-center ">
-          <DolarIcon />
-          {crypto.quote.USD.price > 10 ? crypto.quote.USD.price.toFixed(2) : crypto.quote.USD.price.toFixed(6)}
-        </span>
       </div>
-      <span className={`text-xl md:hidden lg:block ${crypto.quote.USD.percent_change_24h > 0 ? 'text-green-400' : 'text-red-400'}`}>
+      <span className="flex md:hidden lg:flex text-2xl items-center ">
+        <DolarIcon />
+        {crypto.quote.USD.price > 10 ? crypto.quote.USD.price.toFixed(2) : crypto.quote.USD.price.toFixed(6)}
+      </span>
+      <span className={`text-xl flex gap-2 ${crypto.quote.USD.percent_change_24h > 0 ? 'text-green-400' : 'text-red-400'}`}>
         {
-          crypto.quote.USD.percent_change_24h > 0 && <span>+</span>
+          crypto.quote.USD.percent_change_24h > 0
+            ? <TrendingUpIcon />
+            : <TrendingDownIcon />
         }
-        {crypto.quote.USD.percent_change_24h.toFixed(2)}%
+        <span>{crypto.quote.USD.percent_change_24h.toFixed(2)}%</span>
       </span>
     </article>
   )
--- src/modules/listCryptos/components/searchCrypto.tsx
+++ src/modules/listCryptos/components/searchCrypto.tsx
@@ -1,38 +1,41 @@
 import { useEffect, useState } from "react"
-import { data } from "../../core/data/crypto.data";
-import { useCrypto } from "../../core/hooks/useCrypto";
+import { useCrypto, useCryptoSelected } from "../../core/hooks/useCrypto";
 import SearchIcon from "../../../assets/icons/SearchIcon";
+import { DataItem } from "../../core/model/data.mode";
 
 export default function SearchCrypto() {
   const [search, setSearch] = useState("");
+  const { data, loading } = useCrypto()
 
-  const searchData = data.filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
-
-  const { setCryptos, setCryptoSelected } = useCrypto();
-
-  useEffect(() => {
-    setCryptoSelected(searchData[0])
-  }, [])
+  const { setCryptos, setCryptoSelected } = useCryptoSelected();
 
   useEffect(() => {
-    setCryptos(searchData)
-  }, [search])
+    if (!loading && data.length > 0) {
+      setCryptoSelected(data[0])
+    }
+  }, [loading, data, setCryptoSelected]);
 
-  const openList = () => {
-    console.log("pasa")
-  }
+  useEffect(() => {
+    if (!loading && data.length > 0) {
+      const searchData = data.filter((item) =>
+        item.name.toLowerCase().includes(search.toLowerCase())
+      );
+      setCryptos(searchData);
+    }
+  }, [search, loading, data, setCryptos, setCryptoSelected]);
+
 
   return (
     <>
-      <form className="px-8 flex lg:flex md:hidden">
-        <label className="bg-zinc-700 w-full rounded-r-md p-2 flex items-center pl-3 rounded-l-md">
+      <form className="flex w-full">
+        <label className="w-full bg-[#323232]  pl-3 rounded-r-md py-2 flex items-center  rounded-l-md">
           <SearchIcon />
           <input
             type="text"
             placeholder="Search"
             value={search}
             onChange={(e) => setSearch(e.target.value)}
-            className="bg-zinc-700 pl-2 w-full rounded-r-md focus:outline-none"
+            className="bg-[#323232] pl-2 w-full rounded-r-md focus:outline-none"
           />
         </label>
       </form>
