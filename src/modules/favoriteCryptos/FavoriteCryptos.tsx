522d0d0758d97f3d85766911e0ac686ddbfa1155
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-11-22 00:02:44 -0500

    feat: update FavoriteCryptos

--- src/modules/favoriteCryptos/FavoriteCryptos.tsx
+++ src/modules/favoriteCryptos/FavoriteCryptos.tsx
@@ -1,28 +1,42 @@
+import { StarFilledIcon } from "../../assets/icons";
 import DolarIcon from "../../assets/icons/DolarIcon";
-import StarIcon from "../../assets/icons/StarIcon";
 import { Box } from "../../components";
+import { useFavoriteCryptos } from "../../hooks/useFavoriteCryptos";
+import { DataItem } from "../../model/data.mode";
 import { useCryptoSelected } from "../core/hooks/useCrypto";
 
 export default function FavoriteCryptos() {
-  const { cryptos } = useCryptoSelected();
+  const { favoriteCryptos } = useFavoriteCryptos();
+  const { setCryptoSelected } = useCryptoSelected()
+
+  const handleSelectCrypto = (crypto: DataItem) => {
+    setCryptoSelected(crypto)
+  }
+
   return (
-    <section className="pb-8 pr-8">
+    <section className="pb-8 md:pr-8">
       <Box className="pb-0 flex flex-col gap-8">
         <h3 className="text-3xl">Favoritos</h3>
         <div className="flex gap-4 overflow-x-scroll pb-4">
           {
-            cryptos.map((crypto) => (
-              <div className="border-[1px] min-w-56 border-zinc-400 h-min rounded-lg px-2 py-3 flex gap-3 flex-col">
-                <div className="flex justify-between items-center">
-                  <small className="text-nowrap">{crypto.name}</small>
-                  <StarIcon />
+            favoriteCryptos.map((crypto) => (
+              <div
+                key={crypto.id}
+                className="hover:bg-zinc-700/70 hover:cursor-pointer transition-colors border-[1px] min-w-56 border-zinc-400 h-min rounded-lg px-2 py-3 flex gap-3 flex-col"
+                onClick={() => handleSelectCrypto(crypto)}
+              >
+                <div className="flex justify-between items-center text-yellow-400">
+                  <small className="text-nowrap text-white">{crypto.name}</small>
+                  <StarFilledIcon />
                 </div>
                 <div className="flex items-center justify-between">
                   <span className="flex text-2xl items-center">
                     <DolarIcon />
                     {crypto.quote.USD.price > 10 ? crypto.quote.USD.price.toFixed(2) : crypto.quote.USD.price.toFixed(6)}
                   </span>
-                  <small className="border-[1px] rounded-full px-2">{crypto.quote.USD.percent_change_24h.toFixed(2)}%</small>
+                  <small className={`border rounded-full px-2 ${crypto.quote.USD.percent_change_24h > 0 ? 'text-green-400 border-green-400' : 'text-red-400 border-red-400'}`}>
+                    {crypto.quote.USD.percent_change_24h.toFixed(2)}%
+                  </small>
                 </div>
               </div>
             ))
