7bd2a716eb2f0953e7bba9a7f53cd1e024c1c453
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-11-22 00:02:54 -0500

    feat: update ItemCrypto

--- src/modules/listCryptos/components/ItemCrypto.tsx
+++ src/modules/listCryptos/components/ItemCrypto.tsx
@@ -1,9 +1,8 @@
-import { useCallback, useMemo } from "react";
 import DolarIcon from "../../../assets/icons/DolarIcon";
 import { TrendingDownIcon } from "../../../assets/icons/TrendingDownIcon";
 import { TrendingUpIcon } from "../../../assets/icons/TrendingUpIcon";
-import { useCrypto, useCryptoSelected } from "../../core/hooks/useCrypto";
-import { DataItem } from "../../core/model/data.mode";
+import { useCryptoSelected } from "../../core/hooks/useCrypto";
+import { DataItem } from "../../../model/data.mode";
 
 interface ItemCryptoProps {
   crypto: DataItem
@@ -21,9 +20,9 @@ export default function ItemCrypto({ crypto }: ItemCryptoProps) {
   return (
     <article
       onClick={() => selectCrypto()}
-      className={`flex items-center transition-colors gap-4 px-8 py-4 hover:cursor-pointer hover:bg-zinc-700 
+      className={`flex items-center justify-between transition-colors gap-4 px-8 py-4 hover:cursor-pointer hover:bg-zinc-700 
     ${cryptoSelected?.id === crypto.id ? 'bg-zinc-700 sticky top-0' : ''}`}>
-      <div className="flex justify-between w-full  md:justify-center lg:justify-between">
+      <div className="flex justify-between  md:w-full md:justify-center lg:justify-between">
         <div className="flex flex-col md:items-center lg:items-start ">
           <span className="text-xl">{crypto.symbol}</span>
           <small className="text-zinc-400 md:text-center">{crypto.name}</small>
