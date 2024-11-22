faccb3cb2a05c6e3d3ee001611cd1c334e583999
ProtoSg <diegoalbertosalazar177@gmail.com>
2024-11-22 00:02:05 -0500

    feat: update ChartCrypto

--- src/modules/chartCrtypo/ChartCrypto.tsx
+++ src/modules/chartCrtypo/ChartCrypto.tsx
@@ -1,24 +1,45 @@
-import StarIcon from "../../assets/icons/StarIcon";
+import { toast } from "sonner";
+import { StarFilledIcon, StarIcon } from "../../assets/icons";
 import { TrendingDownIcon } from "../../assets/icons/TrendingDownIcon";
 import { TrendingUpIcon } from "../../assets/icons/TrendingUpIcon";
 import { Box } from "../../components";
+import { useFavoriteCryptos } from "../../hooks/useFavoriteCryptos";
 import { useCryptoSelected } from "../core/hooks/useCrypto";
 import ChartLine from "./components/ChartLine";
 import ItemDetail from "./components/ItemDetail";
 
 export default function ChartCrypto() {
   const { cryptoSelected: crypto } = useCryptoSelected();
+  const { addFavoriteCrypto, removeFavoriteCrypto, isFavorite } = useFavoriteCryptos()
 
   if (crypto === null) {
     return <div>Crypto not found</div>
   }
 
+  const favorite = isFavorite(crypto)
+  console.log({ favorite })
+
+  const handleAddFavorite = () => {
+    addFavoriteCrypto(crypto)
+    toast.success('Crypto added to favorites')
+  }
+
+  const handleRemoveFavorite = () => {
+    removeFavoriteCrypto(crypto)
+    toast.success('Crypto removed from favorites')
+  }
+
   return (
-    <section className="flex-1 flex flex-col w-full gap-8 py-8 pr-8">
+    <section className="flex-1 flex flex-col w-full gap-8 py-8 md:pr-8">
       <Box className="relative flex justify-center w-full py-6  border-zinc-600">
         <h3 className="text-3xl font-semibold"> {crypto.name}</h3 >
-        <span className="absolute right-8 top-8">
-          <StarIcon />
+        <span
+          onClick={favorite ? handleRemoveFavorite : handleAddFavorite}
+          className={`absolute right-8 top-8 hover:cursor-pointer hover:text-yellow-400 transition-colors ${favorite ? 'text-yellow-400' : 'text-zinc-400'}`}
+        >
+          {
+            favorite ? <StarFilledIcon /> : <StarIcon />
+          }
         </span>
       </Box >
       <Box className="h-full flex flex-col">
